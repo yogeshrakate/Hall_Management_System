1f66334ea1149ca0d355e5c395b331c8f4884ce8
Yogesh <yogirakate>
2020-07-07 20:57:09 +0530

    first commit

---
+++ guitarApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from guitarApp.models import *
+
+admin.site.register(YoutubeVideos)
+admin.site.register(AboutGuitar)
+admin.site.register(Batches)
+admin.site.register(Feestructure)
+admin.site.register(Buyguitar)
+admin.site.register(Post)
---
+++ guitarApp/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class GuitarappConfig(AppConfig):
+    name = 'guitarApp'
